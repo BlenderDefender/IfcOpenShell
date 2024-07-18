386df5f2a65662d5321674636d4bcfb3abcf090a
Blender Defender <contact.blenderdefender@gmail.com>
2024-07-18 17:00:11 +0200

    chore: More unnecessary whitespace

--- src/blenderbim/blenderbim/bim/__init__.py
+++ src/blenderbim/blenderbim/bim/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with BlenderBIM Add-on.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
+import os 
 import bpy
 import bpy.utils.previews
 import importlib
